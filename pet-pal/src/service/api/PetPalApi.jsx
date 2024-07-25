f971ebee1f9f9c476676825cf626d6f91d726699
dextersjw <dextersjw@gmail.com>
2024-07-26 02:37:42 +0800

    created PetPalService to use both mock and backend server

--- pet-pal/src/service/api/PetPalApi.jsx
+++ pet-pal/src/service/api/PetPalApi.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost";
-const PORT = ":3001";
+const BASE_URL = "http://localhost:3002";
 
-const petPalMockApi = axios.create({ baseURL: BASE_URL + PORT })
+const petPalApi = axios.create({ baseURL: BASE_URL })
 
-export default petPalMockApi;
+export default petPalApi;
