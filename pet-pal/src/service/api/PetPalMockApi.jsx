8e0fb13acdab484a7c2af6cce2008935b3968725
Soh <dexsjw@gmail.com>
2025-01-17 16:57:57 +0800

    changed mockApi to use localhost

--- pet-pal/src/service/api/PetPalMockApi.jsx
+++ pet-pal/src/service/api/PetPalMockApi.jsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 // Do not use localhost, use your machine's IPv4 address instead
 // Reason: Our phone / emulator is not on the same "localhost" as our machine
 
-// const BASE_URL = "http://localhost";
-const BASE_URL = "http://10.91.208.37";
+// const BASE_URL = "http://10.91.208.37";
+const BASE_URL = "http://localhost";
 const PORT = ":3001";
 
 const petPalMockApi = axios.create({ baseURL: BASE_URL + PORT })
